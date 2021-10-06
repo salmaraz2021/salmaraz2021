d762bab0087f87c3c6166f51bb78bb5cf7f74532
salmaraz2021 <s.almaraz.2021@alumnos.urcj.es>
2021-10-06 14:56:04 +0200

    commits de notaii

---
+++ notaii07.pas
@@ -0,0 +1,35 @@
+{$mode objfpc}{$H-}{$R+}{$T+}{$Q+}{$V+}{$D+}{$X-}{$warnings on}
+program nota_ii;
+
+function hace_media(compensable, entrega, teoria, prac: real): boolean;
+begin
+    result :=  (entrega >= compensable) and (teoria >= compensable)
+            and (prac >= compensable)
+end;
+
+function media(peso_entrega, peso_teoria, peso_prac, entrega, teoria, 
+               prac: real): real;
+begin
+    result := entrega * peso_entrega + teoria * peso_teoria + 
+                  prac * peso_prac
+end;
+
+const  // Constantes locales del cuerpo del programa principal
+    Compensable: real = 4.0;
+
+    { Peso relativo de cada apartado de la evaluación, en tanto por uno}
+    Peso_entrega: real = 0.25;    { Entrega prácticas }
+    Peso_teoria: real = 0.35;    { Examen teórico }
+    Peso_prac: real = 0.40;   { Examen práctico }
+
+    Entrega_jose : real = 7.0;  //se ha remplazado Entrega_jperez por Entrega_jose
+    Teoria_jperez: real = 7.0;
+    Prac_jperez: real = 6.5;
+
+begin
+    write('Nota final: ');
+    if (hace_media( Compensable, Entrega_jose, Teoria_jperez, Prac_jperez))
+    then writeln( media(Peso_entrega, Peso_teoria, Peso_prac, Entrega_jose,
+            Teoria_jperez, Prac_jperez):0:2)
+    else writeln( 'No apto' );
+end.
